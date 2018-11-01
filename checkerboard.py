9706d2be320eb670c958dedd11f21c9bf1379b9c
Kyle Dodd <Kylepdodd1@gmail.com>
2018-11-01 12:57:08 -0400

    first commit

---
+++ checkerboard.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route('/<x>/<y>')
+def xy(x, y):
+	return render_template("dynamicBoard.html", x = int(x), y = int(y))
+
+if __name__ == "__main__":
+	app.run(debug=True)
